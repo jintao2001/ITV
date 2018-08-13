868d2fce13667c5b735323a88fe3b92c858bdcc6
wxytkzc <291026174@qq.com>
2018-08-13 21:36:31 +0800

    提交

--- ITVSln/ITV.MvcApplication/App_Start/WebApiConfig.cs
+++ ITVSln/ITV.MvcApplication/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace ITVMvcApplication
+namespace ITV.MvcApplication
 {
     public static class WebApiConfig
     {
