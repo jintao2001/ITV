e7f7cc185c65f17176d49d4ff148415bd234051b
wxytkzc <291026174@qq.com>
2018-08-13 22:31:07 +0800

    提交

--- ITVSln/UnitTest/UnitTest1.cs
+++ ITVSln/UnitTest/UnitTest1.cs
@@ -9,7 +9,8 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
-            
+            var login = new ITV.BLL.BLL_Login();
+            var result = login.LoginValidate("hpb", "hpb");
         }
     }
 }
